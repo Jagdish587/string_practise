cc3fdec0b160e682c0c454274441f2e8ff43d7c3
Jagdish Tirumala <t.jag587@gmail.com>
2018-03-03 01:56:15 +0530

    generilized code
    generilized code

--- swap_bits_in_num.cpp
+++ swap_bits_in_num.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main()
 {
 	int num = 0x34;
-	
+	int p1 = 1;
+	int p2 = 5;
+
 	cout<<"val = "<<bitset<8>(num)<<endl;
 	
-	bool val1 = (num & (0x1<<1)) >> 1 ;
-	bool val2 = (num & (0x1<<4)) >> 4 ; 
+
+	bool val1 = (num & (0x1<<p1)) >> p1 ;
+	bool val2 = (num & (0x1<<p2)) >> p2 ; 
 		
 	
 	if(val1 ^ val2) {
-		int val = (0x1<<1) | (0x1<<4) ;
+		int val = (0x1<<p1) | (0x1<<p2) ;
 		num = num ^ val ;
 	} 
 	
